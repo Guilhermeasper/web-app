a7a19823f5eae6b2442aa0ff7551acff3768cac9
Erick Almeida <Erick2280@users.noreply.github.com>
2025-02-09 18:26:32 -0300

    feat: show account balance type in operation page

--- src/app/pages/operation/operation.component.ts
+++ src/app/pages/operation/operation.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+  computed,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { lucideClock } from '@ng-icons/lucide';
@@ -7,6 +17,8 @@ import {
   HeaderComponent,
   HeaderType,
 } from '@rusbe/components/header/header.component';
+import { AuthStateService } from '@rusbe/services/auth-state/auth-state.service';
+import { GeneralGoodsBalanceType } from '@rusbe/services/general-goods/general-goods.service';
 import {
   MEAL_PRICING_PROFILES,
   RESTAURANT_WORKING_HOURS,
@@ -14,12 +26,69 @@ import {
 
 @Component({
   selector: 'rusbe-operation-page',
-  imports: [HeaderComponent, NgIconComponent],
+  imports: [NgClass, HeaderComponent, NgIconComponent],
   templateUrl: './operation.component.html',
   viewProviders: [provideIcons({ lucideClock })],
 })
-export class OperationPageComponent {
+export class OperationPageComponent implements AfterViewInit {
   readonly HEADER_TYPE = HeaderType.PageNameWithBackButton;
   readonly MEAL_PRICING_PROFILES = MEAL_PRICING_PROFILES;
   readonly RESTAURANT_WORKING_HOURS = RESTAURANT_WORKING_HOURS;
+
+  @ViewChild('pricingProfileCards')
+  pricingProfileCards!: ElementRef<HTMLDivElement>;
+
+  authStateService = inject(AuthStateService);
+
+  isReadyToInteract = signal<boolean>(false);
+
+  userBalanceType = computed(() => {
+    const accountData = this.authStateService.generalGoodsAccountData();
+
+    if (accountData) {
+      return accountData.balance.type;
+    }
+
+    return undefined;
+  });
+
+  constructor() {
+    effect(() => {
+      const isReadyToInteract = this.isReadyToInteract();
+      const balanceType = this.userBalanceType();
+
+      if (isReadyToInteract && balanceType) {
+        this.scrollToMealPriceProfileCard(balanceType);
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.isReadyToInteract.set(true);
+  }
+
+  scrollToMealPriceProfileCard(balanceType: GeneralGoodsBalanceType) {
+    if (this.pricingProfileCards === undefined) {
+      return;
+    }
+
+    const cardIndex = MEAL_PRICING_PROFILES.findIndex(
+      (mealPriceProfile) => mealPriceProfile.balanceType === balanceType,
+    );
+
+    if (cardIndex === -1) {
+      return;
+    }
+
+    const targetCard = this.pricingProfileCards.nativeElement.children[
+      cardIndex
+    ] as HTMLDivElement;
+
+    requestAnimationFrame(() => {
+      this.pricingProfileCards.nativeElement.scrollTo({
+        left: targetCard.offsetLeft,
+        behavior: 'smooth',
+      });
+    });
+  }
 }
